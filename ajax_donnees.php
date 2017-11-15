7f0548ee975a1b5c462d5eeebfadbe3d247fdc48
QuentinDS <quentin.da-silva@telecomnancy.net>
2017-11-15 11:29:53 +0100

    JSON from PHP to JS

--- ajax_donnees.php
+++ ajax_donnees.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 	$db = mysqli_connect('localhost','root','','analyse')
 			or die('Error connecting to MySQL server.');
 
@@ -7,80 +7,67 @@
 	$ageMax = 20;
 	$sexe = 2;
 	$analyseId = 42;
-
-	echo 'Bonjour <br>';
 	
-	$query = 'select valeur from analyse, personne, type_an where ';
+	$query = "select valeur from analyse, personne, type_an where ";
 	$c = 0;
-	// On écrit la requête suivant les données qui sont disponibles
 	if(isset($ville)){
-		$query .= 'ville = '.$ville.' ';
+		$query .= "ville = '$ville' ";
 		$c = 1;
 	}
-	if(isset($ageMin))'={
+	if(isset($ageMin)){
 		if($c){
-			$query .= 'and ';
+			$query .= "and ";
 			$c = 0;
 		}
-		$query .= 'age >= $ageMin ';
+		$query .= "age >= ".$ageMin. " ";
 		$c = 1;
 	}
-	if(isset($ageMax))'={
+	if(isset($ageMax)){
 		if($c){
-			$query .= 'and ';
+			$query .= "and ";
 			$c = 0;
 		}
-		$query .= 'age >= '.$ageMax.' ';
+		$query .= "age <= ".$ageMax." ";
 		$c = 1;
 	}
-	if(isset($sexe))'={
+	if(isset($sexe)){
 		if($c){
-			$query .= 'and ';
+			$query .= "and ";
 			$c = 0;
 		}
-		$query .= 'sexe = '.$sexe.' ';
+		$query .= "sexe = ".$sexe." ";
 		$c = 1;
 	}
-	if(isset($analyseId))'={
+	if(isset($analyseId)){
 		if($c){
-			$query .= 'and ';
+			$query .= "and ";
 			$c = 0;
 		}
-		$query .= 'ref_type = '.$analyseId;
+		$query .= "ref_type = ".$analyseId;
 	}
+	
+	// $personne = query('select valeur, age, sexe, ville from analyse, personne where id_analyse = '$id_analyse' and ref_personne = id_personne'); // On r�cup�re les informations sur la personne � partir des l'id de l'analyse
 	$reponse = mysqli_query($db, $query); /*On recupere toutes les valeurs e toutes les personnes qui correspondent aux donn�es demand�es. Il faut modifier la requ�te en fonction des donn�es manquantes.*/
 
 	$rows = array();
 	while($row = mysqli_fetch_assoc($reponse))
 	{
 		array_push($rows, intval(implode($row)));
-		echo 'Row : ' . intval(implode($row)) . ' ';
 	}
 
-	echo '<br><br>';
-
-	print_r($rows);
-
 	asort($rows); /* Trie r�ponse */
-
-	echo '<br><br>';
-
-	print_r($rows);
-
-	echo '<br><br>';
-
-	$resultsJSON = json_encode($rows);
-
-	$_SESSION['JSON'] = $resultsJSON;
+	
+	$rowsJson = json_encode($rows);
 
 	/*Les lignes ci-dessous sont pertinentes si count($reponse) > 10, environ */
 	$norme_min = $rows[intval((2.5/100)*sizeOf($rows))];// Retourne la valeur � 2.5%
 	$norme_max = $rows[intval((97.5/100)*sizeOf($rows))];// Retourne la valuer � 97.5%
 	/* if (count($rows) >= 10){ Si on a suffisamment de r�sultats sur la ville */
 
-	echo '<br><br>Resultats d\'analyse issus de la commune de : LUXEMBOURG <br>'
-	. 'Le norme pour de tels resutats d\'analyse dans la commune est comprise entre ' . $norme_min . ' et ' . $norme_max . '<br>'
-	. 'Ces resultats ont ete calcules sur ' . sizeOf($rows);
+	$sizeRows = sizeOf($rows);
+	
+	$response = array('dataJson' => $rowsJson, 'normeMin' => $norme_min, 'normeMax' => $norme_max, 'nbRows' => $sizeRows);
+	echo json_encode($response);
 
 	/* } */
 
--- js/controle.js
+++ js/controle.js
@@ -1,12 +1,22 @@
+var dataJson;
+
 $(document).ready(function(){
   $('#result').click(function(){
-    $.ajax({
-      type: 'POST',
-      url: 'ajax_donnees.php',
-      success: function(data) {
-        $("#resultatsPHP").html(data);
-      }
-    });
+	 
+	 $.ajax({
+	   type: "POST",
+	   url: "ajax_donnees.php",
+	   dataType : 'json',
+	   success: function(response){
+			$("#resultatsPHP").html("Json obtenu : " + response.dataJson + "<br><br>" 
+			+ "Norme min : " + response.normeMin + "<br><br>" + "Norme max : " + response.normeMax
+			+ "<br><br>" + "Nombres de rows : " + response.nbRows);
+			var tableau = JSON.parse(response.dataJson);
+			dataJson = tableau;
+			console.log(dataJson);
+	   }
+	});
+	 
   });
   $('input:text').change(function(){
 
