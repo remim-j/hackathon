64f32295d0d4fcca15974cef38e4bf19dafae84c
gauthier demangeon <gauthier.demangeon@telecomnancy.net>
2017-11-16 12:00:23 +0100

    sortie csv

--- ajax_donnees.php
+++ ajax_donnees.php
@@ -1,12 +1,15 @@
 ﻿<?php
 	$db = mysqli_connect('localhost','root','','analyse')
 			or die('Error connecting to MySQL server.');
+$ville = $_POST['ville'];
+$ageMin = $_POST['age_min'];
+$ageMax = $_POST['age_max'];
+$sexe = $_POST['sexe'];
+$analyse = $_POST['type'];
 
-	$ville = $_POST['ville'];
-	$ageMin = $_POST['age_min'];
-	$ageMax = $_POST['age_max'];
-	$sexe = $_POST['sexe'];
-	$analyse = $_POST['type'];
+if(isset($_POST['group']))
+{
+	
 	$reponse = mysqli_query($db, "SELECT id_type FROM type_an WHERE type = '$analyse'");
 	$analyseId = mysqli_fetch_assoc($reponse)['id_type'];
 	
@@ -81,5 +84,61 @@
 	/* } */
 
 	$reponse->close(); /*Termine le traitement de la requ�te*/
+}
+else
+{
+	$chemin = 'fichier.csv';
+	$delimiteur = ',';
 
+	$reponse = mysqli_query($db, "SELECT id_type,norme_min,norme_max FROM type_an WHERE type = '$analyse'");
+	$row = mysqli_fetch_assoc($reponse);
+	$analyseId = $row['id_type'];
+	$norme_min = $row['norme_min'];
+	$norme_max = $row['norme_max'];
+	$query = "select commune,COUNT(*) from analyse, personne,ville_to_commune where ref_personne = id_personne and ville = nom";
+	$queryPlus="";
+	if(!empty($ageMin)){
+		$queryPlus .= " and age >= ".$ageMin. " ";
+	}
+	if(!empty($ageMax)){
+		$queryPlus .= " and age <= ".$ageMax." ";
+	}
+	if(!empty($sexe)){
+		$queryPlus .= " and sexe = ".$sexe." ";
+	}
+	if(!empty($analyseId)){
+		$queryPlus .= " and ref_type = ".$analyseId;
+	}
+	$query.= $queryPlus." GROUP BY commune";
+	$reponseCom = mysqli_query($db, $query);
+	$tab = array();
+	while($row = mysqli_fetch_array($reponseCom))
+	{
+		$query = 'select valeur from analyse, personne,ville_to_commune where ref_personne = id_personne and ville = nom and commune = "'.$row[0].'"' .$queryPlus;
+		$tab[$row[0]] = $row[1];
+		$values = array();
+		if(!($reponse = mysqli_query($db, $query)))
+		{echo $query;return;}
+		while($row2 = mysqli_fetch_array($reponse))
+		{
+			$values[] = $row2[0];
+		}
+		sort($values);
+		$norme_min_calc = $values[intval((2.5/100)*sizeOf($values))];// Retourne la valeur � 2.5%
+		$norme_max_calc = $values[intval((97.5/100)*sizeOf($values))];// Retourne la valuer � 97.5%
+		$tab[$row[0]] = ['count' => $row[1], 'norme_min_calc' => $norme_min_calc, 'norme_max_calc' => $norme_max_calc];
+	}
+	$fichier_csv = fopen($chemin, 'w+');
+	$cpt=1;
+	$header = array('id','Communes','norme_min_calc','count','norme_max_calc','norme_min_calc','norme_max_calc','Cartodb_id','Echelle');
+	fputcsv($fichier_csv, $header, $delimiteur);
+	foreach($tab as $line)
+	{
+		$ligne = array($cpt,array_search($line,$tab),$line['count'],$line['norme_min_calc'],$line['norme_max_calc'],$norme_min,$norme_max,0,0);
+		fputcsv($fichier_csv, $ligne, $delimiteur);
+		$cpt+=1;
+	}
+	fclose($fichier_csv);
+	//echo json_encode(array($tab,$norme_min,$norme_max));
+}
 ?>
